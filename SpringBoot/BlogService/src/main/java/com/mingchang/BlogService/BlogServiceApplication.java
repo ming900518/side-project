0e2c3b5743f4138f128e8ddd1e049b54c85ff35e
ming900518 <ming900518@gmail.com>
2021-08-14 14:43:57 +0800

    [SpringBoot] Add a Spring Boot project.

---
+++ SpringBoot/BlogService/src/main/java/com/mingchang/BlogService/BlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mingchang.BlogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogServiceApplication.class, args);
+	}
+
+}
