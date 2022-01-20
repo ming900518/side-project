f98592dacdc65803c2a22e9228c410265e38c84d
Ming Chang <ming@mingchang137.su>
2022-01-20 17:29:10 +0800

    leftovers.

---
+++ webflux-test/src/main/java/su/mingchang/webfluxtest/WebfluxTestApplication.java
@@ -0,0 +1,13 @@
+package su.mingchang.webfluxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTestApplication.class, args);
+	}
+
+}
