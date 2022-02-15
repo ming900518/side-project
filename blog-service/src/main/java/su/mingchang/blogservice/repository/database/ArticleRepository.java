5362da28e8201293a4b852aa5dd6bc8a27a714a0
Ming Chang <ming@mingchang137.su>
2022-02-15 21:43:03 +0800

    [blog-service] Use Java 15's text block feature in @Query.

--- blog-service/src/main/java/su/mingchang/blogservice/repository/database/ArticleRepository.java
+++ blog-service/src/main/java/su/mingchang/blogservice/repository/database/ArticleRepository.java
@@ -11,22 +11,38 @@ import su.mingchang.blogservice.model.database.Article;
 @Repository
 public interface ArticleRepository extends R2dbcRepository<Article, Integer> {
 
-    @Query("select \"articleId\", " +
-            "\"title\", " +
-            "(select string_agg(\"name\", ', ') from \"tag\" where \"tag\".\"tagId\" in (select \"tagId\" from \"articleTag\" where \"articleTag\".\"articleId\" = \"article\".\"articleId\")) as tags, " +
-            "(select \"name\" from \"admin\" where \"adminId\" = \"article\".\"updateBy\") as updateByName " +
-            "from \"blog\".\"article\" " +
-            "where (:title is null or \"title\" like :title) " +
-            "and (:content is null or \"content\" like :content) " +
-            "and (:updateBy is null or \"updateBy\" = :updateBy);"
-    )
+    @Query("""
+            select article_id,
+                   title,
+                   (select string_agg(name, ', ')
+                    from tag
+                    where tag.tag_id in (select tag_id from article_tag where article_id = article.article_id)) as tags,
+                   (select name from admin where admin_id = article.update_by)                                  as updateByName
+            from blog.article
+            where (:title is null or title like :title)
+              and (:content is null or content like :content)
+              and (:updateBy is null or update_by = :updateBy);
+            """)
     Flux<BackendArticleList> listArticlesBackend(
             String title,
             String content,
             Integer updateBy
     );
 
-    @Query("select \"articleId\", \"title\", (select string_agg(\"name\", ', ') from \"tag\" where \"tag\".\"tagId\" in(select \"tagId\" from \"articleTag\" where \"articleTag\".\"articleId\" = \"article\".\"articleId\")) as tags, concat(substring(content, 0 , 150), '......') as shortContent,(select \"name\" from \"admin\" where \"adminId\" = \"article\".\"updateBy\") as updateByName, \"creationDate\" from \"blog\".\"article\" where \"articleId\" in (select \"articleId\" from \"articleTag\" where (:tagId is null or \"tagId\" = :tagId));")
+    @Query("""
+            select article_id,
+                   title,
+                   (select string_agg(name, ', ')
+                    from tag
+                    where tag.tag_id in (select tag_id from article_tag where article_tag.article_id = article.article_id)) as tags,
+                   case
+                       when length(content) > 150 then concat(substring(content, 0, 150), '......')
+                       else content end                                                                                     as shortContent,
+                   (select name from admin where admin_id = article.update_by)                                              as updateByName,
+                   creation_date
+            from blog.article
+            where article_id in (select article_id from article_tag where (:tagId is null or tag_id = :tagId));
+            """)
     Flux<FrontendArticleList> listArticlesFrontend(
             Integer tagId
     );
